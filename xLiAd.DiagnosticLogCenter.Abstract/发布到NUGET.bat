7a2fda39547c6047c4c48c8c859432f86f0a4f62
zl33842901 <zhanglei33842901@qq.com>
2021-10-19 18:59:42 +0800

    包引用问题修复

--- xLiAd.DiagnosticLogCenter.Abstract/发布到NUGET.bat
+++ xLiAd.DiagnosticLogCenter.Abstract/发布到NUGET.bat
@@ -5,4 +5,5 @@ msbuild.exe   /property:Configuration=Release
 dotnet pack --no-build --output nupkgs --include-symbols
 move nupkgs\*.symbols.nupkg nupkgsSymb\
 D:\nuget push .\nupkgsSymb\* -Source https://api.nuget.org/v3/index.json
+D:\nuget push .\nupkgsSymb\* HjwJmzXbJyzZlgZwLMh -Source http://nuget.cig.com.cn/nuget
 pause
